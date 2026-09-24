bb46ad41d74f58ff61e9725712af203c52fbcd18
mahdyardigry <310025114+mahdyardigry@users.noreply.github.com>
2026-09-24 09:50:26 +0330

    Revert "Remove misplaced V6.4.4 route from getRowCount"
    This reverts commit dab37c85e0f1aa318e614e1dc75da78d692cdcf8.

--- worker.js
+++ worker.js
@@ -2942,6 +2942,13 @@ export class AbsorptionStorageV5 {
   getRowCount() {
     this.initDB();
 
+      if (
+        path === "/internal/v6_4_4/upload" &&
+        request.method === "POST"
+      ) {
+        return this.receiveV644Batch(request);
+      }
+
 
 
     if (
